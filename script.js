296836a9d3bb9d4683f50e8ae62b4be9e1f0e42f
Saleh <justtsalih@gmail.com>
2026-06-23 16:12:48 +0530

    updated css and details

---
+++ script.js
@@ -0,0 +1,27 @@
+// var a = 35;
+// var b = 93;
+// var c = "mohd"
+
+// console.log(a + b);
+// console.log(typeof a, typeof b, typeof c);
+
+// let f = {
+//     name: "Mohd",
+//     age: 25,
+//     "job specs": "soft dev",
+//     "is_handsome": true
+// };
+
+// console.log(f)
+
+// f.salary = "2 thousand"
+// console.log(f)
+
+let age = 18;
+
+if(age>=18){
+    console.log("You can drive")
+}
+else {
+    console.log("you can not drive")
+};
