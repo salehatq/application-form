fa40ccaf39c55a014f89af3ea3ae57022acdde0e
Saleh <justtsalih@gmail.com>
2026-06-29 15:53:36 +0530

    inserted alert message
    Added a getName function to alert the username.

--- script.js
+++ script.js
@@ -1,42 +1,4 @@
-// var a = 35;
-// var b = 93;
-// var c = "mohd"
-
-// console.log(a + b);
-// console.log(typeof a, typeof b, typeof c);
-
-// let f = {
-//     name: "Mohd",
-//     age: 25,
-//     "job specs": "soft dev",
-//     "is_handsome": true
-// };
-
-// console.log(f)
-
-// f.salary = "2 thousand"
-// console.log(f)
-
-// let age = 0;
-
-// if (age == 18) {
-//     console.log("You can drive")
-// }
-
-// else if (age == 0) {
-//     console.log("don't play with me!")
-// }
-
-// else {
-//     console.log("you can not drive")
-// };
-
-// a = 34;
-// b = 23;
-// let c = a > b ? (a - b) : (a + b);
-
-let a = 1;
-
-for (let h = 0; h < 50; h++) {
-    console.log(a + h);
+function getName() {
+    let name = document.getElementById("username").value;
+    alert("Hello, " + name);
 }
