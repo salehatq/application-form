2b378fefb3ddb70b099a9fd9ec9ee2c0d2cf2622
Saleh <justtsalih@gmail.com>
2026-06-29 16:38:05 +0530

    changed design & js

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getName() {
     let name = document.getElementById("username").value;
-    alert("Hello, " + name);
+    alert("Hello, " + name + "\nYour application has been submitted!");
+}
+
+function resetForm() {
+    let name = document.getElementById("username").value;
+    // alert(name + ", your form has been reset!");
+    alert(`${name}, your form has been reset!`)
 }
