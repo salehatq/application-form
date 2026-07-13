38f54ff04d1ba7ffb37fd2f0bdf47c6d831b05d6
Saleh <justtsalih@gmail.com>
2026-07-13 20:01:08 +0530

    updated some information

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function getName() {
     let name = document.getElementById("username").value;
-    alert("Hello, " + name + "\nYour application has been submitted!");
+    alert("Hello " + name + "\nYour application has been submitted!");
 }
 
 function resetForm() {
     let name = document.getElementById("username").value;
     // alert(name + ", your form has been reset!");
-    alert(`${name}, your form has been reset!`)
+    alert(`${name} Your form has been reset!`)
 }
